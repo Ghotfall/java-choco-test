e378908c608d887ba323ecaedd7ca172fca08880
andriimy <Andrii.Myrhorodskyi@nice.com>
2020-12-02 10:06:20 +0200

    Initial

---
+++ src/main/java/com/ghotfall/study_web_app/Server.java
@@ -0,0 +1,24 @@
+package com.ghotfall.study_web_app;
+
+import static spark.Spark.*;
+
+public class Server {
+    private static int counter = 0;
+
+    public static void main(String[] args) {
+        get("/", (request, response) -> {
+            return "Available routes:</br>" +
+                    "/hello/world</br>" +
+                    "/hello/:name</br>" +
+                    "/counter";
+        });
+
+        path("/hello", () -> {
+            get("/world", (request, response) -> "Hello world");
+            get("/:name", (request, response) -> "You are welcome, " + request.params(":name"));
+        });
+
+        get("/counter", (request, response) -> "Counter has been updated. Current value: " + ++counter);
+
+    }
+}
